c6ded70b743c0b80fcebca31bc4ef4c70e5ceeab
asgudao <3418840632@qq.com>
2026-09-10 22:14:55 +0800

    用户模块启动类

---
+++ MiaoWang-backproject/MiaoWang-user/src/main/java/com/MapleLeaf/MiaoWang/UserApplication.java
@@ -0,0 +1,16 @@
+package com.MapleLeaf.MiaoWang;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan(basePackages = "com.MapleLeaf.MiaoWang.dao")
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
