b7bfaa6fafc8e8d92de9bd5a09997cc3293a0fba
asgudao <3418840632@qq.com>
2026-09-14 15:23:16 +0800

    基础依赖

--- MiaoWang-backproject/MiaoWang-ai-pet/src/main/java/com/MapleLeaf/MiaoWang/AiPetApplication.java
+++ MiaoWang-backproject/MiaoWang-ai-pet/src/main/java/com/MapleLeaf/MiaoWang/AiPetApplication.java
@@ -1,6 +1,9 @@
 package com.MapleLeaf.MiaoWang;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class AiPetApplication {
     public static void main(String[] args) {
